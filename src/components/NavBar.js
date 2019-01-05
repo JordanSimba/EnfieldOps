8d163827a043f19b87be744579d23b11721b5c1b
jordan Simbananiye <jordan.simbananiye@accenture.com>
2019-01-05 14:51:58 +0000

    WIP. Need to dynamically change class on nav bar items to set style to "active". Also need to insert logo.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,31 +2,27 @@ import React, { Component } from 'react';
 
 class NavBar extends Component {
     
+    state = {
+        active: false
+    };
+
+    toggleClass() {
+        console.log(this.state.active);
+        let currentState = this.state.active;
+        this.setState({ active: !currentState });
+    };
+
     render() {
         return (
-            // This react fragment is a psuedo tag which can be used in place of divs if we a div is not necessary.
-            <React.Fragment>
+            <nav>
                 <ul className="nav-bar">
-                    <li><a id="nav-item-1" onClick={this.setActive} href="#news">News</a></li>
-                    <li><a id="nav-item-2" onClick={this.setActive} href="#home">Home</a></li>
-                    <li><a id="nav-item-3" onClick={this.setActive} href="#contact">Contact</a></li>
-                    <li><a id="nav-item-4" onClick={this.setActive} href="#about">About</a></li>
+                    <li><a id="nav-item-1" className={this.state.active ? 'active': null} onClick={this.toggleClass} href="#news">Login</a></li>
+                    <li><a id="nav-item-2" className={this.state.active ? 'active': null} onClick={this.toggleClass} href="#home">Sign up</a></li>
                 </ul>
-            </React.Fragment>
+            </nav>
         );
     }
 
-    setActive = (e) => {
-        let id = e.target.id;
-        console.log(id);
-
-        let currentAttr = document.getElementById(id).getAttribute("className");
-        console.log(currentAttr);
-
-        document.getElementById(id).setAttribute("className", "active");
-        console.log(currentAttr);
-
-    }
     // setActive = (e) => {
     //     console.log(e.target.id);
     //     console.log("I'm inside the function!");
