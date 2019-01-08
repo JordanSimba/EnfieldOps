7b769611531d3e08d3ff8ac49d1d0451cf5dd52e
Daniel Jerome <jeromedjp@gmail.com>
2019-01-08 19:13:03 +0000

    First commit of footer-component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div id="Footer">
+        <p>This is the Footer</p>
+      </div>
+    )
+  }
+}
+
+export default Footer
